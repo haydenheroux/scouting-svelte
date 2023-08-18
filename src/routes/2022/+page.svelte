424c0b059da59deb6b8d0575ff84c2be25df4dd8
Hayden Heroux <haydenheroux@gmail.com>
2023-08-18 10:40:51 -0400

    Start working on 2022 page.

---
+++ src/routes/2022/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ParticipantSelector from '$lib/ParticipantSelector.svelte';
+	import AllianceSelector from '$lib/AllianceSelector.svelte';
+	import StartingPosition from './StartingPosition.svelte';
+</script>
+
+<ParticipantSelector />
+<AllianceSelector />
+<StartingPosition />
---
+++ src/routes/2022/StartingPosition.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import field2022 from '$lib/images/fields/2022.jpg';
+</script>
+
+<section>
+    <h2>Starting Position</h2>
+    <img src={field2022} alt="2022 field" />
+</section>
