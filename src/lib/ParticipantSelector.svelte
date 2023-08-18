9b621bf89217c8105fe5afd8205545b79d4f53be
Hayden Heroux <haydenheroux@gmail.com>
2023-08-18 08:36:40 -0400

    Add and style `ParticipantSelector` component.

---
+++ src/lib/ParticipantSelector.svelte
@@ -0,0 +1,16 @@
+<section>
+    <h2>Select Participant</h2>
+    <div>
+        <label for="event">Event</label>
+        <input id="event" type="text" placeholder="2023event">
+    </div>
+    <div>
+        <label for="match">Match</label>
+        <input id="match" type="text" placeholder="qm1">
+    </div>
+    <div>
+        <label for="team">Team</label>
+        <input id="team" type="text" placeholder="5112">
+    </div>
+    <button>Submit</button>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,23 @@
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.
+<script>
+	import ParticipantSelector from '$lib/ParticipantSelector.svelte';
+</script>
+
+<ParticipantSelector />
+
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
