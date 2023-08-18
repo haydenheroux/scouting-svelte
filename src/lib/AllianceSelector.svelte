6f0bb69444c0fbb2c02f08dddaa39e67ae8bfe2b
Hayden Heroux <haydenheroux@gmail.com>
2023-08-18 09:27:19 -0400

    Create `AllianceSelector` component.

---
+++ src/lib/AllianceSelector.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let isRedAlliance = true;
+</script>
+
+<section>
+    <h2>Select Alliance</h2>
+    <div class="split">
+        <button on:click={(e) => isRedAlliance = true} class:red={isRedAlliance}>Red Alliance</button>
+        <button on:click={(e) => isRedAlliance = false} class:blue={!isRedAlliance}>Blue Alliance</button>
+    </div>
+</section>
+
+<style>
+button {
+    background-color: var(--clr-neutral-900);
+    color: var(--clr-neutral-300);
+
+    border: 1px solid var(--clr-neutral-700);
+}
+
+.red {
+    background-color: #E11D48;
+    color: var(--clr-neutral-900);
+}
+
+.blue {
+    background-color: #2563EB;
+    color: var(--clr-neutral-900);
+}
+</style>
