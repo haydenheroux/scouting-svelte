8145faa0764a3ef92dcca2e013d8f6f15872078e
Hayden Heroux <haydenheroux@gmail.com>
2023-08-18 22:53:53 -0400

    fix(MultipleOptionSelector): Double-press clears.

--- src/lib/MultipleOptionSelector.svelte
+++ src/lib/MultipleOptionSelector.svelte
@@ -8,7 +8,7 @@
     <h2>{title}</h2>
     <div class="split">
         {#each options as option}
-            <button on:click={(e) => selected = option} class:active={selected == option}>{option}</button>
+            <button on:click={(e) => selected = (selected != option) ? option : ""} class:active={selected == option}>{option}</button>
         {/each}
     </div>
 </section>
