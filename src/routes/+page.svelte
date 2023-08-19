d5bf5ed141019f7d582c419af3fe6006e61eb80a
Hayden Heroux <haydenheroux@gmail.com>
2023-08-19 10:52:00 -0400

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
-<script>
-	import ParticipantSelector from '$lib/ParticipantSelector.svelte';
-</script>
-
-<ParticipantSelector />
-
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus iusto sint itaque veritatis iste fuga amet quae obcaecati rerum, eius accusamus natus. Commodi architecto aliquam alias distinctio amet, reprehenderit esse.</p>
