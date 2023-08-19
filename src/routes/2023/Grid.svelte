ca1f623895097e4de21f9c61c17c9e265a2e9303
Hayden Heroux <haydenheroux@gmail.com>
2023-08-19 03:45:21 -0400

    fix(2023): Make grid using buttons.

--- src/routes/2023/Grid.svelte
+++ src/routes/2023/Grid.svelte
@@ -15,33 +15,33 @@
 <section>
     <h2>{title}</h2>
     <div class="grid">
-        <div class="cone" class:active={grid[0][0]} on:click={(e) => toggle(0, 0)}></div>
-        <div class="cube" class:active={grid[0][1]} on:click={(e) => toggle(0, 1)}></div>
-        <div class="cone" class:active={grid[0][2]} on:click={(e) => toggle(0, 2)}></div>
-        <div class="cone" class:active={grid[0][3]} on:click={(e) => toggle(0, 3)}></div>
-        <div class="cube" class:active={grid[0][4]} on:click={(e) => toggle(0, 4)}></div>
-        <div class="cone" class:active={grid[0][5]} on:click={(e) => toggle(0, 5)}></div>
-        <div class="cone" class:active={grid[0][6]} on:click={(e) => toggle(0, 6)}></div>
-        <div class="cube" class:active={grid[0][7]} on:click={(e) => toggle(0, 7)}></div>
-        <div class="cone" class:active={grid[0][8]} on:click={(e) => toggle(0, 8)}></div>
-        <div class="cone" class:active={grid[1][0]} on:click={(e) => toggle(1, 0)}></div>
-        <div class="cube" class:active={grid[1][1]} on:click={(e) => toggle(1, 1)}></div>
-        <div class="cone" class:active={grid[1][2]} on:click={(e) => toggle(1, 2)}></div>
-        <div class="cone" class:active={grid[1][3]} on:click={(e) => toggle(1, 3)}></div>
-        <div class="cube" class:active={grid[1][4]} on:click={(e) => toggle(1, 4)}></div>
-        <div class="cone" class:active={grid[1][5]} on:click={(e) => toggle(1, 5)}></div>
-        <div class="cone" class:active={grid[1][6]} on:click={(e) => toggle(1, 6)}></div>
-        <div class="cube" class:active={grid[1][7]} on:click={(e) => toggle(1, 7)}></div>
-        <div class="cone" class:active={grid[1][8]} on:click={(e) => toggle(1, 8)}></div>
-        <div class="floor" class:active={grid[2][0]} on:click={(e) => toggle(2, 0)}></div>
-        <div class="floor" class:active={grid[2][1]} on:click={(e) => toggle(2, 1)}></div>
-        <div class="floor" class:active={grid[2][2]} on:click={(e) => toggle(2, 2)}></div>
-        <div class="floor" class:active={grid[2][3]} on:click={(e) => toggle(2, 3)}></div>
-        <div class="floor" class:active={grid[2][4]} on:click={(e) => toggle(2, 4)}></div>
-        <div class="floor" class:active={grid[2][5]} on:click={(e) => toggle(2, 5)}></div>
-        <div class="floor" class:active={grid[2][6]} on:click={(e) => toggle(2, 6)}></div>
-        <div class="floor" class:active={grid[2][7]} on:click={(e) => toggle(2, 7)}></div>
-        <div class="floor" class:active={grid[2][8]} on:click={(e) => toggle(2, 8)}></div>
+        <button class="cone" class:active={grid[0][0]} on:click={(e) => toggle(0, 0)} />
+        <button class="cube" class:active={grid[0][1]} on:click={(e) => toggle(0, 1)} />
+        <button class="cone" class:active={grid[0][2]} on:click={(e) => toggle(0, 2)} />
+        <button class="cone" class:active={grid[0][3]} on:click={(e) => toggle(0, 3)} />
+        <button class="cube" class:active={grid[0][4]} on:click={(e) => toggle(0, 4)} />
+        <button class="cone" class:active={grid[0][5]} on:click={(e) => toggle(0, 5)} />
+        <button class="cone" class:active={grid[0][6]} on:click={(e) => toggle(0, 6)} />
+        <button class="cube" class:active={grid[0][7]} on:click={(e) => toggle(0, 7)} />
+        <button class="cone" class:active={grid[0][8]} on:click={(e) => toggle(0, 8)} />
+        <button class="cone" class:active={grid[1][0]} on:click={(e) => toggle(1, 0)} />
+        <button class="cube" class:active={grid[1][1]} on:click={(e) => toggle(1, 1)} />
+        <button class="cone" class:active={grid[1][2]} on:click={(e) => toggle(1, 2)} />
+        <button class="cone" class:active={grid[1][3]} on:click={(e) => toggle(1, 3)} />
+        <button class="cube" class:active={grid[1][4]} on:click={(e) => toggle(1, 4)} />
+        <button class="cone" class:active={grid[1][5]} on:click={(e) => toggle(1, 5)} />
+        <button class="cone" class:active={grid[1][6]} on:click={(e) => toggle(1, 6)} />
+        <button class="cube" class:active={grid[1][7]} on:click={(e) => toggle(1, 7)} />
+        <button class="cone" class:active={grid[1][8]} on:click={(e) => toggle(1, 8)} />
+        <button class="floor" class:active={grid[2][0]} on:click={(e) => toggle(2, 0)} />
+        <button class="floor" class:active={grid[2][1]} on:click={(e) => toggle(2, 1)} />
+        <button class="floor" class:active={grid[2][2]} on:click={(e) => toggle(2, 2)} />
+        <button class="floor" class:active={grid[2][3]} on:click={(e) => toggle(2, 3)} />
+        <button class="floor" class:active={grid[2][4]} on:click={(e) => toggle(2, 4)} />
+        <button class="floor" class:active={grid[2][5]} on:click={(e) => toggle(2, 5)} />
+        <button class="floor" class:active={grid[2][6]} on:click={(e) => toggle(2, 6)} />
+        <button class="floor" class:active={grid[2][7]} on:click={(e) => toggle(2, 7)} />
+        <button class="floor" class:active={grid[2][8]} on:click={(e) => toggle(2, 8)} />
     </div>
 </section>
 
@@ -54,37 +54,39 @@
     gap: 0.875rem;
 }
 
-.grid div {
+.grid > * {
+    background-color: var(--clr-neutral-900);
+
     border: 1px solid var(--clr-neutral-700);
 
     aspect-ratio: 1;
 }
 
-.grid div.cube {
+.grid .cube {
     border-radius: var(--radius-element);
 }
 
-.grid div.cube.active {
+.grid .cube.active {
     background-color: #AB17B5;
 
     border: 1px solid #AB17B5;
 }
 
-.grid div.cone {
+.grid .cone {
     border-radius: 50%;
 }
 
-.grid div.cone.active {
+.grid .cone.active {
     background-color: #EFDF0D;
 
     border: 1px solid #EFDF0D;
 }
 
-.grid div.floor {
+.grid .floor {
     border-radius: var(--radius-element);
 }
 
-.grid div.floor.active {
+.grid .floor.active {
     background-color:var(--clr-neutral-100);
 }
 </style>
