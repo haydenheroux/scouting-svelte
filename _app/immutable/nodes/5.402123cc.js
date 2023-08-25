f6cd7fee0b0f6637c1afca58b9743139bb3a7330
Hayden Heroux <haydenheroux@gmail.com>
2023-08-25 09:31:19 -0400

    Updates

---
+++ _app/immutable/nodes/5.402123cc.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.f5c80806.js";import{S as t,i as e}from"../chunks/index.f0de9234.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
---
+++ service-worker.js
@@ -0,0 +1,63 @@
+const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
+  s + "/_app/immutable/entry/app.24e10c82.js",
+  s + "/_app/immutable/assets/0.16679bad.css",
+  s + "/_app/immutable/nodes/0.36fad585.js",
+  s + "/_app/immutable/nodes/1.1cb0ec9c.js",
+  s + "/_app/immutable/nodes/2.dd5d61d7.js",
+  s + "/_app/immutable/assets/3.ed9fc012.css",
+  s + "/_app/immutable/nodes/3.bdf8379f.js",
+  s + "/_app/immutable/assets/2022.ea540ae1.jpg",
+  s + "/_app/immutable/assets/4.5f2922c1.css",
+  s + "/_app/immutable/nodes/4.342b1713.js",
+  s + "/_app/immutable/assets/2023.157ca517.jpg",
+  s + "/_app/immutable/nodes/5.402123cc.js",
+  s + "/_app/immutable/nodes/6.6ea170a8.js",
+  s + "/_app/immutable/assets/Array.a1312612.css",
+  s + "/_app/immutable/chunks/Array.2a51c947.js",
+  s + "/_app/immutable/chunks/index.f0de9234.js",
+  s + "/_app/immutable/chunks/scheduler.f5c80806.js",
+  s + "/_app/immutable/chunks/singletons.f3f46d18.js",
+  s + "/_app/immutable/chunks/stores.0f07acde.js",
+  s + "/_app/immutable/assets/stores.5af87148.css",
+  s + "/_app/immutable/chunks/stores.dbf76f85.js",
+  s + "/_app/immutable/entry/start.727a2fb1.js"
+], o = [
+  s + "/icons/icon-192x192.png",
+  s + "/icons/icon-256x256.png",
+  s + "/icons/icon-384x384.png",
+  s + "/icons/icon-512x512.png",
+  s + "/manifest.webmanifest",
+  s + "/robots.txt"
+], u = "1692970259347", i = `cache-${u}`, p = [
+  ...m,
+  ...o
+];
+self.addEventListener("install", (a) => {
+  async function t() {
+    await (await caches.open(i)).addAll(p);
+  }
+  a.waitUntil(t());
+});
+self.addEventListener("activate", (a) => {
+  async function t() {
+    for (const e of await caches.keys())
+      e !== i && await caches.delete(e);
+  }
+  a.waitUntil(t());
+});
+self.addEventListener("fetch", (a) => {
+  if (a.request.method !== "GET")
+    return;
+  async function t() {
+    const e = new URL(a.request.url), c = await caches.open(i);
+    if (p.includes(e.pathname))
+      return c.match(e.pathname);
+    try {
+      const n = await fetch(a.request);
+      return n.status === 200 && c.put(a.request, n.clone()), n;
+    } catch {
+      return c.match(a.request);
+    }
+  }
+  a.respondWith(t());
+});
