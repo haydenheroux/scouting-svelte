a3dcf81acd26aed57689733c2a2ee2d0603acf5b
Hayden Heroux <haydenheroux@gmail.com>
2023-08-25 11:07:34 -0400

    Revert previous commit.

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,12 @@
 <script>
     import NavLink from '$lib/components/NavLink.svelte';
 
-    import { base } from '$app/paths';
+    import { assets, base } from '$app/paths';
 </script>
 
 <header>
     <div class="container">
         <nav>
-            <a href="{base}/"><img src="{base}/icons/icon-512x512.png" alt="Logo"></a>
             <a href="{base}/"><em>scouting-svelte</em></a>
             <NavLink href="{base}/2024">2024</NavLink>
             <NavLink href="{base}/2023">2023</NavLink>
