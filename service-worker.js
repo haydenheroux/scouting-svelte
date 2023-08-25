f0d35f8d0138b7e8dbeb7c32704c695c6c96dd6f
Hayden Heroux <haydenheroux@gmail.com>
2023-08-25 12:04:33 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
-  s + "/_app/immutable/entry/app.34bcc8c7.js",
-  s + "/_app/immutable/assets/0.b9daa55c.css",
-  s + "/_app/immutable/nodes/0.99f1ec16.js",
-  s + "/_app/immutable/nodes/1.e120dcc2.js",
+  s + "/_app/immutable/entry/app.5b5fd60d.js",
+  s + "/_app/immutable/assets/0.56f9550e.css",
+  s + "/_app/immutable/nodes/0.36df6522.js",
+  s + "/_app/immutable/nodes/1.e56f4e56.js",
   s + "/_app/immutable/nodes/2.dd5d61d7.js",
   s + "/_app/immutable/assets/3.ed9fc012.css",
-  s + "/_app/immutable/nodes/3.bdf8379f.js",
+  s + "/_app/immutable/nodes/3.81bd6d10.js",
   s + "/_app/immutable/assets/2022.ea540ae1.jpg",
-  s + "/_app/immutable/assets/4.5f2922c1.css",
-  s + "/_app/immutable/nodes/4.342b1713.js",
+  s + "/_app/immutable/assets/4.f9aa665c.css",
+  s + "/_app/immutable/nodes/4.158040ae.js",
   s + "/_app/immutable/assets/2023.157ca517.jpg",
   s + "/_app/immutable/nodes/5.402123cc.js",
   s + "/_app/immutable/nodes/6.6ea170a8.js",
   s + "/_app/immutable/assets/Array.a1312612.css",
-  s + "/_app/immutable/chunks/Array.2a51c947.js",
+  s + "/_app/immutable/chunks/Array.0a01108a.js",
   s + "/_app/immutable/chunks/index.f0de9234.js",
   s + "/_app/immutable/chunks/scheduler.f5c80806.js",
-  s + "/_app/immutable/chunks/singletons.10418a37.js",
+  s + "/_app/immutable/chunks/singletons.27e0e6c3.js",
   s + "/_app/immutable/chunks/stores.0f07acde.js",
   s + "/_app/immutable/assets/stores.5af87148.css",
-  s + "/_app/immutable/chunks/stores.769d93dd.js",
-  s + "/_app/immutable/entry/start.64c86beb.js"
+  s + "/_app/immutable/chunks/stores.0fbbd161.js",
+  s + "/_app/immutable/entry/start.2d0d787e.js"
 ], o = [
   s + "/.nojekyll",
   s + "/icons/icon-192x192.png",
@@ -29,7 +29,7 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m
   s + "/icons/icon-512x512.png",
   s + "/manifest.webmanifest",
   s + "/robots.txt"
-], l = "1692976066256", i = `cache-${l}`, p = [
+], l = "1692979452552", i = `cache-${l}`, p = [
   ...m,
   ...o
 ];
@@ -50,14 +50,14 @@ self.addEventListener("fetch", (a) => {
   if (a.request.method !== "GET")
     return;
   async function t() {
-    const e = new URL(a.request.url), c = await caches.open(i);
+    const e = new URL(a.request.url), n = await caches.open(i);
     if (p.includes(e.pathname))
-      return c.match(e.pathname);
+      return n.match(e.pathname);
     try {
-      const n = await fetch(a.request);
-      return n.status === 200 && c.put(a.request, n.clone()), n;
+      const c = await fetch(a.request);
+      return c.status === 200 && n.put(a.request, c.clone()), c;
     } catch {
-      return c.match(a.request);
+      return n.match(a.request);
     }
   }
   a.respondWith(t());
