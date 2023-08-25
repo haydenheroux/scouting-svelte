411b37865bee5355d9e4465b0f807180930f8c76
Hayden Heroux <haydenheroux@gmail.com>
2023-08-25 10:18:28 -0400

    feat: Add script to deploy to Github Pages.

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,15 +1,17 @@
 <script>
     import NavLink from '$lib/components/NavLink.svelte';
+
+    import { base } from '$app/paths';
 </script>
 
 <header>
     <div class="container">
         <nav>
-            <a href="/"><em>scouting-svelte</em></a>
-            <NavLink href="/2024">2024</NavLink>
-            <NavLink href="/2023">2023</NavLink>
-            <NavLink href="/2022">2022</NavLink>
-            <NavLink href="/data">Data</NavLink>
+            <a href="{base}/"><em>scouting-svelte</em></a>
+            <NavLink href="{base}/2024">2024</NavLink>
+            <NavLink href="{base}/2023">2023</NavLink>
+            <NavLink href="{base}/2022">2022</NavLink>
+            <NavLink href="{base}/data">Data</NavLink>
         </nav>
     </div>
 </header>
