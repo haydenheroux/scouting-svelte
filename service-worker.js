852831507a6c7787253cf9e1b6fa155dfa72ffa7
Hayden Heroux <haydenheroux@gmail.com>
2023-08-27 13:30:28 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,32 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
-  a + "/_app/immutable/entry/app.62ba68fb.js",
+  a + "/_app/immutable/entry/app.18f74c13.js",
   a + "/_app/immutable/assets/0.ff8cab10.css",
-  a + "/_app/immutable/nodes/0.e1f22cb6.js",
-  a + "/_app/immutable/nodes/1.6c95cb9b.js",
+  a + "/_app/immutable/nodes/0.2ff0e677.js",
+  a + "/_app/immutable/nodes/1.620f805d.js",
   a + "/_app/immutable/assets/2.e0e9b41e.css",
-  a + "/_app/immutable/nodes/2.658a2728.js",
+  a + "/_app/immutable/nodes/2.e9611f41.js",
   a + "/_app/immutable/assets/3.ed9fc012.css",
-  a + "/_app/immutable/nodes/3.d033d78d.js",
+  a + "/_app/immutable/nodes/3.fcf2fa13.js",
   a + "/_app/immutable/assets/2022.ea540ae1.jpg",
   a + "/_app/immutable/assets/4.f9aa665c.css",
-  a + "/_app/immutable/nodes/4.79e0e7c9.js",
+  a + "/_app/immutable/nodes/4.b8c075dc.js",
   a + "/_app/immutable/assets/2023.157ca517.jpg",
   a + "/_app/immutable/nodes/5.6c08f0d6.js",
-  a + "/_app/immutable/nodes/6.22890a1f.js",
+  a + "/_app/immutable/nodes/6.a0fff141.js",
   a + "/_app/immutable/assets/ParticipantSelector.63f29628.css",
   a + "/_app/immutable/assets/QRCodeDisplay.5e98f770.css",
   a + "/_app/immutable/assets/Serialized.f67b3dcb.css",
   a + "/_app/immutable/chunks/ParticipantSelector.222b0b51.js",
-  a + "/_app/immutable/chunks/QRCodeDisplay.b30523a9.js",
+  a + "/_app/immutable/chunks/QRCodeDisplay.67d77d61.js",
   a + "/_app/immutable/chunks/Serialized.186bdc36.js",
-  a + "/_app/immutable/chunks/array.95eba9fd.js",
+  a + "/_app/immutable/chunks/array.cdcd90bc.js",
   a + "/_app/immutable/assets/array.20e86b96.css",
   a + "/_app/immutable/chunks/index.8ad9ff12.js",
   a + "/_app/immutable/chunks/index.dcec8585.js",
   a + "/_app/immutable/chunks/scheduler.cc2b1be1.js",
-  a + "/_app/immutable/chunks/singletons.8053428b.js",
-  a + "/_app/immutable/chunks/stores.9aae1748.js",
-  a + "/_app/immutable/entry/start.b97c2f04.js"
+  a + "/_app/immutable/chunks/singletons.31695ef2.js",
+  a + "/_app/immutable/chunks/stores.5ea1bd7a.js",
+  a + "/_app/immutable/entry/start.7a3a7354.js"
 ], l = [
   a + "/.nojekyll",
   a + "/icons/icon-192x192.png",
@@ -35,7 +35,7 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m
   a + "/icons/icon-512x512.png",
   a + "/manifest.webmanifest",
   a + "/robots.txt"
-], u = "1693156623761", n = `cache-${u}`, p = [
+], u = "1693157399897", n = `cache-${u}`, p = [
   ...m,
   ...l
 ];
