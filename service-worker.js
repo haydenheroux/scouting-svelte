6801b1a9458678669c187f4a0e178ac23ddf83c7
Hayden Heroux <haydenheroux@gmail.com>
2023-08-28 10:02:22 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
-  a + "/_app/immutable/entry/app.ff4cf9fe.js",
+  a + "/_app/immutable/entry/app.571a666a.js",
   a + "/_app/immutable/assets/0.ccedaa50.css",
-  a + "/_app/immutable/nodes/0.e5e71be5.js",
-  a + "/_app/immutable/nodes/1.14a9bb8e.js",
+  a + "/_app/immutable/nodes/0.2b65faa9.js",
+  a + "/_app/immutable/nodes/1.a430a7dd.js",
   a + "/_app/immutable/nodes/2.31737a41.js",
   a + "/_app/immutable/assets/3.ed9fc012.css",
   a + "/_app/immutable/nodes/3.2fbf128b.js",
@@ -13,7 +13,7 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m
   a + "/_app/immutable/nodes/5.6c08f0d6.js",
   a + "/_app/immutable/nodes/6.89f1a03a.js",
   a + "/_app/immutable/assets/7.568b7da3.css",
-  a + "/_app/immutable/nodes/7.f0f0c0ee.js",
+  a + "/_app/immutable/nodes/7.8cf160a9.js",
   a + "/_app/immutable/assets/ParticipantSelector.29b96609.css",
   a + "/_app/immutable/assets/QRCodeDisplay.5e98f770.css",
   a + "/_app/immutable/chunks/ParticipantSelector.85f4921b.js",
@@ -25,11 +25,11 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m
   a + "/_app/immutable/chunks/index.8ad9ff12.js",
   a + "/_app/immutable/chunks/index.dcec8585.js",
   a + "/_app/immutable/chunks/scheduler.cc2b1be1.js",
-  a + "/_app/immutable/chunks/singletons.b6482640.js",
+  a + "/_app/immutable/chunks/singletons.bd9baed0.js",
+  a + "/_app/immutable/chunks/stores.1917ae76.js",
   a + "/_app/immutable/chunks/stores.2b7084df.js",
   a + "/_app/immutable/assets/stores.f67b3dcb.css",
-  a + "/_app/immutable/chunks/stores.88358a58.js",
-  a + "/_app/immutable/entry/start.afecd465.js"
+  a + "/_app/immutable/entry/start.f0f50ceb.js"
 ], l = [
   a + "/.nojekyll",
   a + "/icons/icon-192x192.png",
@@ -38,7 +38,7 @@ const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m
   a + "/icons/icon-512x512.png",
   a + "/manifest.webmanifest",
   a + "/robots.txt"
-], u = "1693231201662", p = `cache-${u}`, i = [
+], u = "1693231311181", p = `cache-${u}`, i = [
   ...m,
   ...l
 ];
