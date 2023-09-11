b5c2fefae50b36de3733abfc01f4cc4a79944692
Hayden Heroux <haydenheroux@gmail.com>
2023-09-11 13:58:15 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,31 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
-  s + "/_app/immutable/entry/app.c690b0c1.js",
-  s + "/_app/immutable/assets/0.0f2348e2.css",
-  s + "/_app/immutable/nodes/0.4ccf59e6.js",
-  s + "/_app/immutable/nodes/1.a97b1c2c.js",
+  s + "/_app/immutable/entry/app.2dfd69b8.js",
+  s + "/_app/immutable/assets/0.4a3e72f3.css",
+  s + "/_app/immutable/nodes/0.0c3b94db.js",
+  s + "/_app/immutable/nodes/1.58a43e31.js",
   s + "/_app/immutable/nodes/2.402123cc.js",
-  s + "/_app/immutable/assets/3.ed9fc012.css",
-  s + "/_app/immutable/nodes/3.e5dec929.js",
+  s + "/_app/immutable/assets/3.f574c9fe.css",
+  s + "/_app/immutable/nodes/3.21fc1e58.js",
   s + "/_app/immutable/assets/2022.ea540ae1.jpg",
-  s + "/_app/immutable/assets/4.4b985043.css",
-  s + "/_app/immutable/nodes/4.bf3ad556.js",
+  s + "/_app/immutable/assets/4.9b7e2df4.css",
+  s + "/_app/immutable/nodes/4.5a5d785b.js",
   s + "/_app/immutable/assets/2023.157ca517.jpg",
   s + "/_app/immutable/nodes/5.402123cc.js",
-  s + "/_app/immutable/nodes/6.78dceaae.js",
-  s + "/_app/immutable/assets/7.568b7da3.css",
-  s + "/_app/immutable/nodes/7.5dd4dedd.js",
-  s + "/_app/immutable/assets/QRCodeDisplay.5e98f770.css",
-  s + "/_app/immutable/chunks/QRCodeDisplay.78fa1f71.js",
-  s + "/_app/immutable/chunks/_commonjsHelpers.db51605f.js",
-  s + "/_app/immutable/assets/_commonjsHelpers.f67b3dcb.css",
-  s + "/_app/immutable/chunks/array.01fb68ac.js",
-  s + "/_app/immutable/assets/array.04fdd944.css",
+  s + "/_app/immutable/nodes/6.2c9c61a5.js",
+  s + "/_app/immutable/assets/7.94ab062d.css",
+  s + "/_app/immutable/nodes/7.78026692.js",
+  s + "/_app/immutable/assets/QRCodeDisplay.5406a6c2.css",
+  s + "/_app/immutable/chunks/QRCodeDisplay.4a2dd712.js",
+  s + "/_app/immutable/chunks/_commonjsHelpers.c1e8e212.js",
+  s + "/_app/immutable/assets/_commonjsHelpers.9f5f45fb.css",
+  s + "/_app/immutable/chunks/array.f66511a3.js",
+  s + "/_app/immutable/assets/array.a36a1326.css",
   s + "/_app/immutable/chunks/index.a3c86b5d.js",
   s + "/_app/immutable/chunks/index.f0de9234.js",
   s + "/_app/immutable/chunks/scheduler.f5c80806.js",
-  s + "/_app/immutable/chunks/singletons.c0af8056.js",
-  s + "/_app/immutable/chunks/stores.5dedb444.js",
-  s + "/_app/immutable/entry/start.54124943.js"
+  s + "/_app/immutable/chunks/singletons.d950eeb9.js",
+  s + "/_app/immutable/chunks/stores.a5f5887e.js",
+  s + "/_app/immutable/entry/start.bcba5cca.js"
 ], l = [
   s + "/.nojekyll",
   s + "/icons/icon-192x192.png",
@@ -34,10 +34,7 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m
   s + "/icons/icon-512x512.png",
   s + "/manifest.webmanifest",
   s + "/robots.txt"
-], o = "1693787359981", p = `cache-${o}`, i = [
-  ...m,
-  ...l
-];
+], o = "1694455060130", p = `cache-${o}`, i = [...m, ...l];
 self.addEventListener("install", (a) => {
   async function t() {
     await (await caches.open(p)).addAll(i);
