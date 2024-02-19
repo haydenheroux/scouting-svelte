1a3982c86939559ccc507dd729a9c548a9e0c1ce
Hayden Heroux <haydenheroux@gmail.com>
2024-02-19 12:32:42 -0500

    refactor: Change how match metrics are stored.

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,16 +1,34 @@
 import { createDefaultParticipant, type Participant } from '$lib/participant';
-import { serialize, type SerializedParticipantMetrics } from './api';
+import { getMatchCode, serialize, type SerializedAlliance, type SerializedMatchCode, type SerializedParticipantMetrics } from './api';
 import type { MatchMetrics } from './metrics';
 import { storable } from './storable';
 
 export const storedParticipant = storable<Participant>('participant', createDefaultParticipant());
 
-export const storedMetrics = storable<Array<SerializedParticipantMetrics>>('metrics', [] as Array<SerializedParticipantMetrics>);
+export const storedMatches = storable<MatchToAlliance>('matches', {} as MatchToAlliance);
+
+type MatchToAlliance = Record<SerializedMatchCode, AllianceToStations>;
+type AllianceToStations = Record<SerializedAlliance, StationToMetrics>;
+type StationToMetrics = Record<number, Array<SerializedParticipantMetrics>>;
 
 export function serializeAndStore(participant: Participant, metrics: MatchMetrics): SerializedParticipantMetrics {
-		const serialized = serialize(participant, metrics);
+    const serialized = serialize(participant, metrics);
 
-		storedMetrics.set([...storedMetrics.get(), serialized]);
+    const allMatches = storedMatches.get();
 
-        return serialized;
+    const matchCode = getMatchCode(serialized.participant);
+
+    const matchAlliances = allMatches[matchCode] || {};
+    const allianceStations = matchAlliances[serialized.participant.alliance] || {};
+    const stationMetrics = allianceStations[participant.station] || [];
+
+    stationMetrics.push(serialized);
+
+    allianceStations[participant.station] = stationMetrics;
+    matchAlliances[serialized.participant.alliance] = allianceStations;
+    allMatches[matchCode] = matchAlliances;
+
+    storedMatches.set(allMatches);
+
+    return serialized;
 }
--- src/routes/matches/+page.svelte
+++ src/routes/matches/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import QRCodeDisplay from '$lib/components/sections/QRCodeDisplay.svelte';
 	import Section from '$lib/components/Section.svelte';
-	import { storedMetrics } from '$lib/stores';
+	import { storedMatches } from '$lib/stores';
 	import { getMatchCode } from '$lib/api';
 	import { deserializeAlliance } from '$lib/adapter';
 
-	let history = storedMetrics.get();
+	let history = storedMatches.get();
 </script>
 
 {#each history as report}
