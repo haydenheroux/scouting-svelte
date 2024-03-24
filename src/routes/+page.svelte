ceb48673000a01003b47b453231f34a803764157
Hayden Heroux <haydenheroux@gmail.com>
2024-03-24 14:34:28 -0400

    refactor: Use modal for submit.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import MetricsQRCode from "$lib/components/sections/MetricsQRCode.svelte";
 	import { storeMetrics } from "$lib/stores";
 	import MetricsSelector from "$lib/components/sections/MetricsSelector.svelte";
 	import Section from "$lib/components/Section.svelte";
 	import type { Metrics } from "$lib/metrics";
+	import { Modal, Content, Trigger } from "sv-popup";
+	import MetricsQrCode from "$lib/components/sections/MetricsQRCode.svelte";
 
 	let metrics: Metrics = {} as Metrics;
 
@@ -19,8 +20,12 @@
 <MetricsSelector bind:metrics />
 
 <Section name="Submit">
-	<button class="primary" on:click={handleSubmit}>Submit</button>
-	{#if showQRCode}
-		<MetricsQRCode bind:metrics />
-	{/if}
+	<Modal small button={false}>
+		<Content>
+			<MetricsQrCode {metrics} />
+		</Content>
+		<Trigger>
+			<button class="primary" on:click={handleSubmit}>Submit</button>
+		</Trigger>
+	</Modal>
 </Section>
